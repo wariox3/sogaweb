18533dfe486df4082c9333d5ba9ad04d44462326
jefedesarrollo <jefedesarrollo@jefedesarrollo-H81M-H>
2016-02-16 08:59:27 -0500

    Actualizacion

--- general/menu.php
+++ general/menu.php
@@ -5,7 +5,7 @@
                                 <li id="nav1" class="active"><a href="<?php echo $strRutaVacia; ?>index.php">Inicio<span>Bienvenido!</span></a></li>
                                 <li id="nav2"><a href="<?php echo $strRutaVacia; ?>noticias.php">Nuevo<span>Actualizar</span></a></li>
                                 <li id="nav3"><a href="<?php echo $strRutaVacia; ?>documentacion.php">Ayuda<span>Documentacion</span></a></li>
-                                <li id="nav4"><a href="<?php echo $strRutaVacia; ?>productos.php">Productos<span>Los mejores</span></a></li>
+                                <li id="nav4"><a href="<?php echo $strRutaVacia; ?>productos.php">Productos<span>Servicios</span></a></li>
                                 <li id="nav5"><a href="<?php echo $strRutaVacia; ?>contactenos.php">Contactenos<span>Email</span></a></li>
                         </ul>
                 </nav>
--- productos.php
+++ productos.php
@@ -24,46 +24,13 @@
                 <!-- content -->
                 <article id="content">
                     <div class="wrapper">
-                        <section class="cols">
-                            <div class="wrapper pad_bot2">
-                                <h3><span class="dropcap">1</span>Turnos</h3>
-                                <figure><img src="images/page4_img1.jpg" alt=""></figure>
-                                <p class="pad_bot1">Video tutoriales y contenido para el manejo del modulo turnos. Documentacion relacionada.</p>
-                                <a href="documentacion/turno/lista.php" class="link1">Leer mas</a>
-                            </div>
+                        <section class="col1">
+                            <h2 class="under">Recurso humano!</h2>
                             <div class="wrapper">
-                                <h3><span class="dropcap">4</span>Transporte</h3>
-                                <figure><img src="images/page4_img2.jpg" alt=""></figure>
-                                <p class="pad_bot1">Losgistica de transporte, documentacion de la aplicacion y los procesos que maneja el sistema.</p>
-                                <a href="#" class="link1">Leer mas</a>
-                            </div>
-                        </section>
-                        <section class="cols pad_left1">
-                            <div class="wrapper pad_bot2">
-                                <h3><span class="dropcap">2</span>Contabilidad</h3>
-                                <figure><img src="images/page4_img3.jpg" alt=""></figure>
-                                <p class="pad_bot1">Documentacion de la apliacion y recursos de tutoria para manejar la contabilidad son soga app.</p>
-                                <a href="#" class="link1">Leer mas</a>
-                            </div>
-                            <div class="wrapper">
-                                <h3><span class="dropcap">5</span>Herramientas</h3>
-                                <figure><img src="images/page4_img4.jpg" alt=""></figure>
-                                <p class="pad_bot1">Instructivo para manejo de herramientas tipo general, calendario, tareas, proyectos, gestor de archivos entre otras.</p>
-                                <a href="#" class="link1">Leer mas</a>
-                            </div>
-                        </section>
-                        <section class="cols pad_left1">
-                            <div class="wrapper pad_bot2">
-                                <h3><span class="dropcap">3</span>Recurso humano</h3>
-                                <figure><img src="images/page4_img5.jpg" alt=""></figure>
-                                <p class="pad_bot1">Paquete de documentacion del modulo de recursos humanos, normatividad y actualizaciones.</p>
-                                <a href="documentacion/recurso_humano/lista.php" class="link1">Leer mas</a>
-                            </div>
-                            <div class="wrapper">
-                                <h3><span class="dropcap">6</span>Inventario</h3>
-                                <figure><img src="images/page4_img6.jpg" alt=""></figure>
-                                <p class="pad_bot1">Procesos de inventario, instructivos y manuales de uso de la aplicacion, nuevas tecnicas de costeco y actualizaciones.</p>
-                                <a href="#" class="link1">Leer mas</a>
+                                <figure class="left marg_right1"><img src="images/page1_img1.jpg" alt=""></figure>
+                                <p class="pad_bot1">La aplicacion de recurso humano.</p>
+                                <p>
+                                    El uso de herramientas que trabajan sobre cualquier sistema operativo y las cuales son de uso libre.</p>
                             </div>
                         </section>
                     </div>
