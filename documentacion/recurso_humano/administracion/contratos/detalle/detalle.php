388301c0506f70890c78ca9c568cfc606174e9bb
pc--25 <pc--25@pc25-H81M-H>
2016-09-24 10:12:32 -0500

    Actualizacion 08 Modulo administracion - contratos finalizado.

--- documentacion/recurso_humano/administracion/contratos/detalle/detalle.php
+++ documentacion/recurso_humano/administracion/contratos/detalle/detalle.php
@@ -34,8 +34,49 @@
                                         Luego se nos desplegara la siguiente ventana. <br> <br>
                                         <img src="../../../../../images/recurso_humano/contrato/contrato14.jpg"  alt="" width="920"/> <br> <br>
                                         
-                                        Al ingresar a esta ventana tenemos las siguientes opciones
-
+                                        En la parte inferior de la pantalla encontraremos 4 opciones que son las siguientes: <br><br>
+                                        
+                                        La primera de ellas es el cambio de salario. Esta opción nos permitirá programar un cambio en el salario a partir de una fecha en específico. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato15.jpg"  alt="" width="920"/> <br><br>
+                                        
+                                        Vamos a la opción nuevo y se nos desplegara la siguiente ventana. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato16.jpg"  alt=""/> <br>
+                                        
+                                        Agregamos el nuevo salario y la fecha en la cual se hará vigente el cambio. <br>
+                                        Agregamos el detalle o el motivo del cambio y finalizamos con la opción guardar. <br> <br>
+                                        
+                                        La segunda opción son las vacaciones. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato17.jpg"  alt="" width="920"/> <br> <br>
+                                        
+                                        A través de esta opción vamos a observar las vacaciones que se le programan al empleado con su respectiva informacion detallada. <br><br>
+                                        
+                                        La tercer opción es el cambio de sede del contrato.<br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato18.jpg"  alt="" width="920"/> <br><br>
+                                        
+                                        Para realizar el procedimiento vamos al botón nuevo ubicado en la parte inferior de la pantalla y se nos desplegara la siguiente ventana. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato19.jpg"  alt="" /> <br>
+                                        
+                                        Seleccionamos la sede a la que aplicaremos el cambio y finalizamos con el boton guardar. <br><br><br>
+                                        
+                                        La cuarta y ultima opción son los traslados de la entidad de pension y salud.<br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato20.jpg"  alt="" width="920"/> <br><br>
+                                        
+                                        Para realizar el procedimiento solo vamos a la opción nuevo segun el traslado que vamos a realizar y se nos desplegara la siguiente ventana. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato21.jpg"  alt=""/> <br>
+                                        
+                                        Solo llenamos los campos que nos solicitan y finalizamos el proceso con el boton guardar. <br><br>
+                                        
+                                        <img src="../../../../../images/recurso_humano/contrato/contrato22.jpg"  alt=""/> <br><br>
+                                        
+                                        Para el proceso de la entidad de salud también llenamos los datos que nos solicitan pero debemos tener presento que es posible que se requiera cambiar la entidad de salud de los familiares que estén afiliados. <br>
+                                        
                                     </li>
                                 </ul>    
                             </div>                            
