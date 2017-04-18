6981260c28d564927fc90f33cc4af3a7a3b56db9
pc--25 <pc--25@pc25-H81M-H>
2017-04-18 14:43:11 -0500

    soga web

--- general/cuerpo3.php
+++ general/cuerpo3.php
@@ -1,118 +1,118 @@
           
-        <div class="main">               
-            <!-- content -->
-            <article id="content">
-                <div class="wrapper">
-                    <section class="cols">
-                        <h3><span class="dropcap">A</span>Planifica<span>negocios</span></h3>
-                        <p class="pad_bot1">Herramientas con un enfoque de <a href="#" target="_blank">proyeccion</a> creadas para soportar las decisiones a futuro de su negocio.</p>
-                        <a href="#" class="link1">Leer mas</a>
-                    </section>
-                    <section class="cols pad_left1">
-                        <h3><span class="dropcap">B</span>Crea<span>estrategias</span></h3>
-                        <p class="pad_bot1">No solo diseñamos <a href="#" target="_blank" rel="nofollow">Herramientas y utilidades</a> para satisfacer los procesos, estamos pensando siempre en hacerlo mas facil y rapido.</p>
-                        <a href="#" class="link1">Leer mas</a>
-                    </section>
-                    <section class="cols pad_left1">
-                        <h3><span class="dropcap">C</span>Analisis<span>fuerte</span></h3>
-                        <p class="pad_bot1">Reportes, listas, excel, informes detallados de cada movimiento de su negocio.</p>
-                        <a href="#" class="link1">Leer mas</a>
-                    </section>
-                    <section class="cols pad_left1">
-                        <h3><span class="dropcap">D</span>Solucion<span>web</span></h3>
-                        <p class="pad_bot1">Compartimos su pensamiento: Internet ya hace parte de nosotros, es el presente y el futuro, diseñamos herramientas para esto.</p>
-                        <a href="#" class="link1">Leer mas</a>
-                    </section>
-                </div>
-                <div class="wrapper">
-                    <section class="col1">
-                        <h2 class="under">Bienvenido, estamos innovando!</h2>
-                        <div class="wrapper">
-                            <figure class="left marg_right1"><img src="images/innovacion.jpg" alt="" width="200"></figure>
-                            <p class="pad_bot1">Compatibilidad, rendimiento, desempeño, agilidad son adjetivos dentro de cada uno de los desarrollos asumidos por el equipo de Soga, la satisfaccion del cliente se ve reflejada en el uso de nuestras aplicaciones hechas con los ultimos estandares de calidad y en las mejores herramientas de desarrollo.</p>
-                            <p>
-                                El uso de herramientas que trabajan sobre cualquier sistema operativo y las cuales son de uso libre evita la transferencia de costos indirectos a los clientes que no necesitan depender de alguna marca o sistema operativo en particular.</p>
-                        </div>
-                    </section>
-                    <section class="col2 pad_left1">
-                        <h2>Testimonio</h2>
-                        <div class="testimonials">
-                            <div id="testimonials">
-                                <ul>
-                                    <li>
-                                        <div>
-                                            “La programacion a traves de SogaApp nos dismunuye el trabajo de una gran manera.”
-                                        </div>
-                                        <span><strong class="color1">Pablo Aranzazu</strong> <br>
-                                            Analista de sistemas</span>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            “Nam libero tempore, cum soluta nobis eligendi quo minus quod maxime placeat facere.”
-                                        </div>
-                                        <span><strong class="color1">James Coloway,</strong> <br>
-                                            Director</span>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            “Nam libero tempore, cum soluta nobis eligendi quo minus quod maxime placeat facere.”
-                                        </div>
-                                        <span><strong class="color1">James Coloway,</strong> <br>
-                                            Director</span>
-                                    </li>
-                                </ul>
-                            </div>
-                            <a href="#" class="up"></a>
-                            <a href="#" class="down"></a>
-                        </div>
-                    </section>
-                    <section class="col1">
-                        <h2 class="under">Novedas SogaApp</h2>
-                        <div class="wrapper">
-                            <figure class="left marg_right1"><img src="images/mejoras.png" alt="" width="200" ></figure>
-                            <p class="pad_bot1">El equipo de desarrollo de SogaApp esta en una constante evolucion que nos permita construir una aplicacion que cumpla con todas las exigencias de nuestros usuarios.<br><br> En este apartado se estaran publicando las ultimas actualizaciones que se realicen en nuestra aplicacion</p>
-                            <p>
-                            <table class="col1">
-                                <tr class="color1">
-                                     <td>Tipo</td>
-                                     <td>Fecha</td>
-                                     <td>Modulo</td>
-                                     <td>Comentarios</td>
-                                   </tr>
-                                   <tr>
-                                     <td>Mejora</td>
-                                     <td>2016-11-24</td>
-                                     <td>Recurso Humano</td>
-                                     <td>Las programaciones de pago no pueden ser pagadas si uno de sus pagos tiene el valor neto menor que cero</td>
-                                   </tr>
-                                   <tr>
-                                     <td>Correccion</td>
-                                     <td>2016-11-24</td>
-                                     <td>Recurso Humano</td>
-                                     <td>Cuando se liquida una programacion de pago se actualizan los valores netos del detalle y el numero de pagos que contiene</td>
-                                   </tr>
-                                   <tr>
-                                     <td>Nueva herramienta</td>
-                                     <td>2016-11-24</td>
-                                     <td>Recurso Humano</td>
-                                     <td>Registro de reclamaciones de los empleados para casos de nomina</td>
-                                   </tr>
-                                   <tr>
-                                     <td>Nueva herramienta</td>
-                                     <td>2016-11-24</td>
-                                     <td>Inventario</td>
-                                     <td>Avanza el modulo de inventario</td>
-                                   </tr>
-                                   <tr>
-                                     <td>Cambio Menu</td>
-                                     <td>2016-11-24</td>
-                                     <td>Recurso HUmano</td>
-                                     <td>El acceso a la aplicacion de creditos cambio: Movimiento -> Recurso humano -> Nomina -> Creditos</td>
-                                   </tr>                                   
-                               </table>
-                        </div>
-                    </section>
-                </div>
-            </article>
+<div class="main">               
+    <!-- content -->
+    <article id="content">
+        <div class="wrapper">
+            <section class="cols">
+                <h3><span class="dropcap">A</span>Planifica<span>negocios</span></h3>
+                <p class="pad_bot1">Herramientas con un enfoque de <a href="#" target="_blank">proyeccion</a> creadas para soportar las decisiones a futuro de su negocio.</p>
+                <a href="#" class="link1">Leer mas</a>
+            </section>
+            <section class="cols pad_left1">
+                <h3><span class="dropcap">B</span>Crea<span>estrategias</span></h3>
+                <p class="pad_bot1">No solo diseñamos <a href="#" target="_blank" rel="nofollow">Herramientas y utilidades</a> para satisfacer los procesos, estamos pensando siempre en hacerlo mas facil y rapido.</p>
+                <a href="#" class="link1">Leer mas</a>
+            </section>
+            <section class="cols pad_left1">
+                <h3><span class="dropcap">C</span>Analisis<span>fuerte</span></h3>
+                <p class="pad_bot1">Reportes, listas, excel, informes detallados de cada movimiento de su negocio.</p>
+                <a href="#" class="link1">Leer mas</a>
+            </section>
+            <section class="cols pad_left1">
+                <h3><span class="dropcap">D</span>Solucion<span>web</span></h3>
+                <p class="pad_bot1">Compartimos su pensamiento: Internet ya hace parte de nosotros, es el presente y el futuro, diseñamos herramientas para esto.</p>
+                <a href="#" class="link1">Leer mas</a>
+            </section>
         </div>
+        <div class="wrapper">
+            <section class="col1">
+                <h2 class="under">Bienvenido, estamos innovando!</h2>
+                <div class="wrapper">
+                    <figure class="left marg_right1"><img src="images/innovacion.jpg" alt="" width="200"></figure>
+                    <p class="pad_bot1">Compatibilidad, rendimiento, desempeño, agilidad son adjetivos dentro de cada uno de los desarrollos asumidos por el equipo de Soga, la satisfaccion del cliente se ve reflejada en el uso de nuestras aplicaciones hechas con los ultimos estandares de calidad y en las mejores herramientas de desarrollo.</p>
+                    <p>
+                        El uso de herramientas que trabajan sobre cualquier sistema operativo y las cuales son de uso libre evita la transferencia de costos indirectos a los clientes que no necesitan depender de alguna marca o sistema operativo en particular.</p>
+                </div>
+            </section>
+            <section class="col2 pad_left1">
+                <h2>Testimonio</h2>
+                <div class="testimonials">
+                    <div id="testimonials">
+                        <ul>
+                            <li>
+                                <div>
+                                    “La programacion a traves de SogaApp nos dismunuye el trabajo de una gran manera.”
+                                </div>
+                                <span><strong class="color1">Pablo Aranzazu</strong> <br>
+                                    Analista de sistemas</span>
+                            </li>
+                            <li>
+                                <div>
+                                    “Nam libero tempore, cum soluta nobis eligendi quo minus quod maxime placeat facere.”
+                                </div>
+                                <span><strong class="color1">James Coloway,</strong> <br>
+                                    Director</span>
+                            </li>
+                            <li>
+                                <div>
+                                    “Nam libero tempore, cum soluta nobis eligendi quo minus quod maxime placeat facere.”
+                                </div>
+                                <span><strong class="color1">James Coloway,</strong> <br>
+                                    Director</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <a href="#" class="up"></a>
+                    <a href="#" class="down"></a>
+                </div>
+            </section>
+            <section class="col1">
+                <h2 class="under">Novedas SogaApp</h2>
+                <div class="wrapper">
+                    <figure class="left marg_right1"><img src="images/mejoras.png" alt="" width="200" ></figure>
+                    <p class="pad_bot1">El equipo de desarrollo de SogaApp esta en una constante evolucion que nos permita construir una aplicacion que cumpla con todas las exigencias de nuestros usuarios.<br><br> En este apartado se estaran publicando las ultimas actualizaciones que se realicen en nuestra aplicacion</p>
+                    <p>
+                    <table class="col1">
+                        <tr class="color1">
+                            <td>Tipo</td>
+                            <td>Fecha</td>
+                            <td>Modulo</td>
+                            <td>Comentarios</td>
+                        </tr>
+                        <tr>
+                            <td>Mejora</td>
+                            <td>2016-11-24</td>
+                            <td>Recurso Humano</td>
+                            <td>Las programaciones de pago no pueden ser pagadas si uno de sus pagos tiene el valor neto menor que cero</td>
+                        </tr>
+                        <tr>
+                            <td>Correccion</td>
+                            <td>2016-11-24</td>
+                            <td>Recurso Humano</td>
+                            <td>Cuando se liquida una programacion de pago se actualizan los valores netos del detalle y el numero de pagos que contiene</td>
+                        </tr>
+                        <tr>
+                            <td>Nueva herramienta</td>
+                            <td>2016-11-24</td>
+                            <td>Recurso Humano</td>
+                            <td>Registro de reclamaciones de los empleados para casos de nomina</td>
+                        </tr>
+                        <tr>
+                            <td>Nueva herramienta</td>
+                            <td>2016-11-24</td>
+                            <td>Inventario</td>
+                            <td>Avanza el modulo de inventario</td>
+                        </tr>
+                        <tr>
+                            <td>Cambio Menu</td>
+                            <td>2016-11-24</td>
+                            <td>Recurso HUmano</td>
+                            <td>El acceso a la aplicacion de creditos cambio: Movimiento -> Recurso humano -> Nomina -> Creditos</td>
+                        </tr>                                   
+                    </table>
+                </div>
+            </section>
+        </div>
+    </article>
+</div>
 </html>
--- general/menu.php
+++ general/menu.php
@@ -1,13 +1,13 @@
-                <div class="wrapper">
-                <h1><a href="<?php echo $strRutaVacia; ?>index.php" id="logo">App Soga</a></h1>
-                <nav>
-                        <ul id="menu">
-                                <li id="nav1" class="active"><a href="<?php echo $strRutaVacia; ?>index.php">Inicio<span>Bienvenido!</span></a></li>
-                                <li id="nav2"><a href="<?php echo $strRutaVacia; ?>cliente.php">Cliente<span>Acceso</span></a></li>
-                                <li id="nav3"><a href="<?php echo $strRutaVacia; ?>documentacion.php">Ayuda<span>Documentacion</span></a></li>
-                                <li id="nav4"><a href="<?php echo $strRutaVacia; ?>productos.php">Producto<span>Servicios</span></a></li>
-                                <li id="nav5"><a href="<?php echo $strRutaVacia; ?>contactenos.php">Contacto<span>Email</span></a></li>
-                        </ul>
-                </nav>
-                </div>
+<div class="wrapper">
+    <h1><a href="<?php echo $strRutaVacia; ?>index.php" id="logo">App Soga</a></h1>
+    <nav>
+        <ul id="menu">
+            <li id="nav1" class="active"><a href="<?php echo $strRutaVacia; ?>index.php">Inicio<span>Bienvenido!</span></a></li>
+            <li id="nav2"><a href="<?php echo $strRutaVacia; ?>cliente.php">Cliente<span>Acceso</span></a></li>
+            <li id="nav3"><a href="<?php echo $strRutaVacia; ?>documentacion.php">Ayuda<span>Documentacion</span></a></li>
+            <li id="nav4"><a href="<?php echo $strRutaVacia; ?>productos.php">Producto<span>Servicios</span></a></li>
+            <li id="nav5"><a href="<?php echo $strRutaVacia; ?>contactenos.php">Contacto<span>Email</span></a></li>
+        </ul>
+    </nav>
+</div>
 
