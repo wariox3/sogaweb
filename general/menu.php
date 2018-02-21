27c71ce3c49ea162c83a67c602a5bb13051a1801
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-02-21 17:19:37 -0500

    Update menu.php
    Que no se pueda acceder a contactenos

--- general/menu.php
+++ general/menu.php
@@ -6,7 +6,7 @@
             <li id="nav2"><a href="<?php echo $strRutaVacia; ?>cliente.php">Cliente<span>Acceso</span></a></li>
             <li id="nav3"><a href="<?php echo $strRutaVacia; ?>documentacion.php">Ayuda<span>Documentacion</span></a></li>
             <li id="nav4"><a href="<?php echo $strRutaVacia; ?>productos.php">Producto<span>Servicios</span></a></li>
-            <li id="nav5"><a href="<?php echo $strRutaVacia; ?>contactenos.php">Contacto<span>Email</span></a></li>
+            <li id="nav5"><a href="#">Contacto<span>Email</span></a></li>
         </ul>
     </nav>
 </div>
